82945cc45b9fc6db2c3bf2d6d88d48c0bee797cd
Malik Glossop <malik.glossop@gmail.com>
2018-08-16 00:10:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function createRock(x) {
  */
 function endGame() {
   gameInterval = null
-  body.remove('#dodger')
+  document.remove('#dodger')
   ROCKS.remove('.rocks')
   // need to remove ROCKS Still
   alert("YOU LOSE!")
