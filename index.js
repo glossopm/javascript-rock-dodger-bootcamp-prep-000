71f74cc2f611fc36db37b0f146ab8789d2f9c670
Malik Glossop <malik.glossop@gmail.com>
2018-08-16 01:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ function createRock(x) {
         rock.style.top = `${top += 2}px`
         if (top < 400) {
           return window.requestAnimationFrame(moveRock)
+        }
         else {
           ROCKS.remove(rock)
         }
@@ -105,7 +106,7 @@ function createRock(x) {
   }
 
   // We should kick of the animation of the rock around here
-  window.requestAnimationFrame(moveRock)
+
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
